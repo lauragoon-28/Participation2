00ca4b2e9445c82c1bd0da53e9d4be697db44e97
Laura Goon <Laura.goon@ou.edu>
2021-02-04 17:35:20 -0600

    Added a solution and named a variable, "MAGIC_NUMBER"

---
+++ SumOf3/SumOf3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SumOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double MAGIC_Number = 7.777;// local constant variable
+
+
+
+        }
+    }
+}
