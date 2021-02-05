6d39df6fa6d9614f737e9a28e0e0dbed00eef525
Laura Goon <Laura.goon@ou.edu>
2021-02-04 18:14:45 -0600

    Finished Code for Participation

--- SumOf3/SumOf3/Program.cs
+++ SumOf3/SumOf3/Program.cs
@@ -8,6 +8,27 @@ namespace SumOf3
         {
             const double MAGIC_Number = 7.777;// local constant variable
 
+            Double Number1;
+            Console.WriteLine("Enter a number between 1 and 5.5 >>");
+            String Number1AsString = Console.ReadLine();
+            Number1 = Convert.ToDouble(Number1AsString);
+
+            Double Number2;
+            Console.WriteLine("Enter a second number between 1 and 5.5>>");
+            String Number2AsString = Console.ReadLine();
+            Number2 = Convert.ToDouble(Number2AsString);
+
+            Double Number3;
+            Console.WriteLine("Enter a third number between 1 and 5.5>>");
+            String Number3AsString = Console.ReadLine();
+            Number2 = Convert.ToDouble(Number3AsString);
+
+            double Total = Number1 + Number2 + Number3;
+            double Answer = Total * MAGIC_Number;
+            string Final = Answer.ToString("F3");
+            Console.WriteLine(Final);
+
+
 
 
         }
